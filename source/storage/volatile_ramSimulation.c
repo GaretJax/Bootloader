873894598fbd0c5809f9de52a8bb4bea935dbda9
Elias Medawar <eliasmedawar@ubuntu.ubuntu-domain>
2010-05-12 15:34:57 +0200

    The ram api using the local ram as storage support. 	- Assume that the ram size = 256KByte

---
+++ source/storage/volatile_ramSimulation.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include "./volatile.h"
+char VS_RAM[256000];
+int vstorage_read(char * buffer,const void * relativeADR, int length){
+    char *add = VS_RAM + (int)relativeADR;
+    memcpy ( buffer, add, length);
+    return 0;
+}
+int vstorage_write(const char * buffer, void * relativeADR, int length){ 
+    char *dst  =VS_RAM + (int)relativeADR; 
+    memcpy (dst, buffer, length);
+    return 0;
+}
+int main(){
+    void * add = (void *)0x000A;
+    char te[] = "Valeur en mémoire à l'adresse 10";
+    vstorage_write(te,add,strlen(te)+1);
+    add = (void *)0x002A;
+    char te2[] = "Valeur à l'adresse 42";
+    add = (void *)0x000A;
+    vstorage_read(te2,add,34);
+    printf("Read %s",te2);
+    return 0;
+}
