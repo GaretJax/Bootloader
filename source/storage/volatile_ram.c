7b6d93ace0abf322a5d4926bc7222235bd2dc11c
Elias Medawar <eliasmedawar@ubuntu.ubuntu-domain>
2010-05-12 15:33:12 +0200

    The appi to use on the uProcessor. 	- Asume that the ram base  @ = 0x100000

--- source/storage/volatile_ram.c
+++ source/storage/volatile_ram.c
@@ -1,9 +1,15 @@
 #include <stdio.h> 
-#include "storage/volatile.h"
-int vstorage_read(char * buffer,const void * address, int length){
-    memcpy ( buffer, address, length);
+#include <stdlib.h> 
+#include <string.h>
+#include "./volatile.h"
+char * RAM_BASE = (char *)0x100000;
+int vstorage_read(char * buffer,const void * relativeADR, int length){
+    char *add = RAM_BASE + (int)relativeADR;
+    memcpy ( buffer, add, length);
     return 0;
 }
-int vstorage_write(const char * buffer, void * address, int length){
-    memcpy ( address, buffer, length);
+int vstorage_write(const char * buffer, void * relativeADR, int length){ 
+    char *dst  = RAM_BASE + (int)relativeADR; 
+    memcpy (dst, buffer, length);
+    return 0;
 }
