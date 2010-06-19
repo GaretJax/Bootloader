83a8aa98b012081dea63c2fc9190af4406e4eac6
yves <yves@ifuntu.(none)>
2010-06-19 09:22:28 +0200

    basic implementation. not tested

--- source/configuration/registry.c
+++ source/configuration/registry.c
@@ -9,12 +9,12 @@
 
 
 int config_save() {
-	int res = pstorage_write(keys, 0, sizeof(keys));
+	int res = pstorage_write(config, 0, sizeof(keys));
 	return res;
 }
 
 int config_load() {
-	int res = pstorage_read(keys, 0, sizeof(keys));
+	int res = pstorage_read(config, 0, sizeof(keys));
 	return res;
 }
 
